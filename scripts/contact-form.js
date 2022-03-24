42627abf03d5bcf7b7604d2b81460955b789a1b7
Emily Tegg <emilytegg58@gmail.com>
2022-03-24 17:55:06 +0000

    add comments

--- scripts/contact-form.js
+++ scripts/contact-form.js
@@ -1,13 +1,21 @@
+/* 
+    handles submission + validation of contact form 
+ */
+
+//get form element
 let form = document.querySelector('[data-provider="contact-form"')
 
+//get children we care about from form element
 let nameInput = form.querySelector('#name')
 let email = form.querySelector('#email')
 let submit = form.querySelector('[type="submit"]')
 
+//attach event listeners
 function attachEvents() {
     submit.addEventListener("click", handleSubmit)
 }
 
+//handle submit click
 function handleSubmit() {
     let nameValue = nameInput.value
     let emailValue = email.value
@@ -18,9 +26,11 @@ function handleSubmit() {
     }
 }
 
+//validate fields data
 function validate(name, email) {
     let emailValid = email && email.indexOf("@") > 0
     return name && emailValid
 }
 
+//attach to page on DOMContentLoaded
 document.addEventListener('DOMContentLoaded', attachEvents)
